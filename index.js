4bfe663639c9030aea549c70245e9cf1c8c92cb9
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-03 16:35:19 -0600

    math phase 1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Write your code herele
+let num1 = 31
+let num2 =2
+let multiply = num1 * num2 
+
+
+
+let random = + 1;
+
+let num3 = 12
+let num4 = 3
+
+let mod = num3 / num4
+
+let max = Math.max(1,6,17,20)
+
